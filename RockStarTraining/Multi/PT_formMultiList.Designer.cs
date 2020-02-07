149f0d7f81ba6901699b07949a100e24878266e2
test.migration <test.migration@rockstar-gym.com>
2020-02-07 15:41:32 +0700

    add new formMultiList

---
+++ RockStarTraining/Multi/PT_formMultiList.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace RockStar.Training
+{
+    partial class PT_formMultiList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_ClubName = new DevExpress.XtraEditors.LabelControl();
+            this.alertControl1 = new DevExpress.XtraBars.Alerter.AlertControl();
+            this.pictureEdit_Logo = new DevExpress.XtraEditors.PictureEdit();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.splashScreenManager1 = new DevExpress.XtraSplashScreen.SplashScreenManager(this, typeof(global::RockStar.Training.form_Wait), true, true);
+            this.btn_StartScan = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_Done = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit_Logo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_ClubName
+            // 
+            this.lb_ClubName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_ClubName.Appearance.Font = new System.Drawing.Font("Tahoma", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lb_ClubName.Appearance.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lb_ClubName.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            this.lb_ClubName.Appearance.TextOptions.VAlignment = DevExpress.Utils.VertAlignment.Top;
+            this.lb_ClubName.AutoSizeMode = DevExpress.XtraEditors.LabelAutoSizeMode.None;
+            this.lb_ClubName.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.lb_ClubName.Location = new System.Drawing.Point(344, 12);
+            this.lb_ClubName.Name = "lb_ClubName";
+            this.lb_ClubName.Size = new System.Drawing.Size(419, 73);
+            this.lb_ClubName.TabIndex = 69;
+            this.lb_ClubName.Text = "Kota Kasablanka";
+            // 
+            // alertControl1
+            // 
+            this.alertControl1.AllowHotTrack = false;
+            this.alertControl1.AutoFormDelay = 5000;
+            this.alertControl1.FormMaxCount = 1;
+            this.alertControl1.FormShowingEffect = DevExpress.XtraBars.Alerter.AlertFormShowingEffect.SlideHorizontal;
+            this.alertControl1.LookAndFeel.SkinName = "Office 2010 Blue";
+            this.alertControl1.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.alertControl1.ShowCloseButton = false;
+            this.alertControl1.ShowPinButton = false;
+            this.alertControl1.ShowToolTips = false;
+            this.alertControl1.BeforeFormShow += new DevExpress.XtraBars.Alerter.AlertFormEventHandler(this.alertControl1_BeforeFormShow);
+            // 
+            // pictureEdit_Logo
+            // 
+            this.pictureEdit_Logo.Location = new System.Drawing.Point(12, 12);
+            this.pictureEdit_Logo.Name = "pictureEdit_Logo";
+            this.pictureEdit_Logo.Properties.AllowFocused = false;
+            this.pictureEdit_Logo.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit_Logo.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit_Logo.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pictureEdit_Logo.Properties.ShowMenu = false;
+            this.pictureEdit_Logo.Size = new System.Drawing.Size(72, 41);
+            this.pictureEdit_Logo.TabIndex = 83;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridControl1.Location = new System.Drawing.Point(3, 101);
+            this.gridControl1.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Flat;
+            this.gridControl1.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(769, 274);
+            this.gridControl1.TabIndex = 85;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Appearance.HeaderPanel.BackColor = System.Drawing.Color.White;
+            this.gridView1.Appearance.HeaderPanel.BackColor2 = System.Drawing.Color.CornflowerBlue;
+            this.gridView1.Appearance.HeaderPanel.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.gridView1.Appearance.HeaderPanel.Options.UseBackColor = true;
+            this.gridView1.Appearance.HeaderPanel.Options.UseTextOptions = true;
+            this.gridView1.Appearance.HeaderPanel.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridView1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.gridView1.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsBehavior.Editable = false;
+            this.gridView1.OptionsMenu.EnableColumnMenu = false;
+            this.gridView1.OptionsMenu.EnableFooterMenu = false;
+            this.gridView1.OptionsMenu.EnableGroupPanelMenu = false;
+            this.gridView1.OptionsPrint.AutoWidth = false;
+            this.gridView1.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView1.OptionsView.ColumnAutoWidth = false;
+            this.gridView1.OptionsView.ShowFilterPanelMode = DevExpress.XtraGrid.Views.Base.ShowFilterPanelMode.Never;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.OptionsView.ShowIndicator = false;
+            this.gridView1.CustomDrawColumnHeader += new DevExpress.XtraGrid.Views.Grid.ColumnHeaderCustomDrawEventHandler(this.gridView1_CustomDrawColumnHeader);
+            // 
+            // btn_StartScan
+            // 
+            this.btn_StartScan.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_StartScan.Appearance.Options.UseFont = true;
+            this.btn_StartScan.Location = new System.Drawing.Point(117, 29);
+            this.btn_StartScan.Name = "btn_StartScan";
+            this.btn_StartScan.Size = new System.Drawing.Size(87, 56);
+            this.btn_StartScan.TabIndex = 88;
+            this.btn_StartScan.Text = "Start Scan";
+            this.btn_StartScan.Click += new System.EventHandler(this.btn_StartScan_Click);
+            // 
+            // btn_Done
+            // 
+            this.btn_Done.Appearance.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Done.Appearance.Options.UseFont = true;
+            this.btn_Done.Location = new System.Drawing.Point(238, 29);
+            this.btn_Done.Name = "btn_Done";
+            this.btn_Done.Size = new System.Drawing.Size(87, 56);
+            this.btn_Done.TabIndex = 89;
+            this.btn_Done.Text = "Done";
+            this.btn_Done.Click += new System.EventHandler(this.btn_Done_Click);
+            // 
+            // PT_formMultiList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 378);
+            this.Controls.Add(this.btn_Done);
+            this.Controls.Add(this.btn_StartScan);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.pictureEdit_Logo);
+            this.Controls.Add(this.lb_ClubName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "PT_formMultiList";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PT_formMultiList";
+            this.Load += new System.EventHandler(this.PT_formMultiList_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PT_formMultiList_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PT_formMultiList_KeyDown);
+            this.Resize += new System.EventHandler(this.PT_formMultiList_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit_Logo.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraBars.Alerter.AlertControl alertControl1;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit_Logo;
+        private DevExpress.XtraEditors.LabelControl lb_ClubName;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.SimpleButton btn_StartScan;
+        private DevExpress.XtraEditors.SimpleButton btn_Done;
+        private DevExpress.XtraSplashScreen.SplashScreenManager splashScreenManager1;
+    }
+}
---
+++ RockStarTraining/Multi/PT_formMultiList.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using Axioma.Celebrity.Fitness;
+using System.Media;
+
+namespace RockStar.Training
+{
+    public partial class PT_formMultiList : Form
+    {
+        SqlConnection myConnection = new SqlConnection(Partner.configConnection);
+     
+        Bitmap gbr_inf = new Bitmap(Properties.Resources.info_icon, 25, 25);
+        Bitmap gbr_warn = new Bitmap(Properties.Resources.warning, 25, 25);
+        Bitmap gbr_error = new Bitmap(Properties.Resources.close, 25, 25);
+           
+
+        DataTable dt_finger_employees;
+        private string code_clubUser;
+        private string code_UserClubName;
+        private bool isFinger;
+
+        /// <summary>
+        /// Input Datatable if using finger, if not null
+        /// </summary>
+        /// <param name="clubName"></param>
+        /// <param name="code_clubUser"></param>
+        /// <param name="dt_finger_employees"></param>        
+        public PT_formMultiList(string code_UserClubName, string code_clubUser,DataTable dt_finger_employees, bool isFinger)
+        {
+            InitializeComponent();
+
+            this.code_UserClubName = code_UserClubName;
+            this.dt_finger_employees = new DataTable();
+            this.dt_finger_employees = dt_finger_employees;
+            this.code_clubUser = code_clubUser;
+            this.isFinger = isFinger;
+        }
+
+        private void PT_formMultiList_Load(object sender, EventArgs e)
+        {
+            lb_ClubName.Text = code_UserClubName;
+            Bitmap logo = new Bitmap(Properties.Resources.Logo_RSG);
+            pictureEdit_Logo.Image = logo;
+        }
+
+        //======= agar form yang muncul, bisa di drag
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);
+        }
+  
+      
+
+        private void alertControl1_BeforeFormShow(object sender, DevExpress.XtraBars.Alerter.AlertFormEventArgs e)
+        {
+            e.AlertForm.Height = 100;
+            e.AlertForm.Width = 300;
+            e.AlertForm.OpacityLevel = 3;
+        }
+
+        private void PT_formMultiList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            }
+        }
+
+        private void PT_formMultiList_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, this.ClientRectangle, Color.FromArgb(29, 26, 77), 2, ButtonBorderStyle.Solid, Color.FromArgb(29, 26, 77), 2, ButtonBorderStyle.Solid, Color.FromArgb(29, 26, 77), 2, ButtonBorderStyle.Solid, Color.FromArgb(29, 26, 77), 2, ButtonBorderStyle.Solid);
+        }
+
+        private void PT_formMultiList_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+     
+
+        private void arrangeCol()
+        {
+            if (gridControl1.DataSource != null)
+            {
+                gridView1.Columns["counter"].VisibleIndex = 0;
+                gridView1.Columns["counter"].Caption = "Counter";
+                gridView1.Columns["counter"].Width = 50;
+
+                gridView1.Columns["memberName"].VisibleIndex = 1;
+                gridView1.Columns["memberName"].Caption = "Student Name";
+                gridView1.Columns["memberName"].Width = 200;
+
+                gridView1.Columns["productName"].VisibleIndex = 2;
+                gridView1.Columns["productName"].Caption = "Product Name";
+                gridView1.Columns["productName"].Width = 200;
+
+                gridView1.Columns["instructorName"].VisibleIndex = 3;
+                gridView1.Columns["instructorName"].Caption = "Instructor Name";
+                gridView1.Columns["instructorName"].Width = 150;
+
+                gridView1.Columns["remain"].VisibleIndex = 4;
+                gridView1.Columns["remain"].Caption = "Remain";
+                gridView1.Columns["remain"].Width = 50;
+
+                gridView1.Columns["expired"].VisibleIndex = 5;
+                gridView1.Columns["expired"].Caption = "Expired Date";
+                gridView1.Columns["expired"].DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+                gridView1.Columns["expired"].DisplayFormat.FormatString = "dd MMM yyyy";
+                gridView1.Columns["expired"].Width = 100;
+
+                gridView1.Columns["note"].VisibleIndex = 6;
+                gridView1.Columns["note"].Caption = "Note";
+                gridView1.Columns["note"].Width = 200;
+
+
+                gridView1.Columns["approveBy"].Visible = false;
+                gridView1.Columns["approveDate"].Visible = false;
+                gridView1.Columns["clubName"].Visible = false;
+                gridView1.Columns["code"].Visible = false;
+                gridView1.Columns["barcode"].Visible = false;
+                gridView1.Columns["agreement"].Visible = false;
+                gridView1.Columns["firstName"].Visible = false;
+                gridView1.Columns["lastName"].Visible = false;
+            }
+        }
+
+        private void gridView1_CustomDrawColumnHeader(object sender, DevExpress.XtraGrid.Views.Grid.ColumnHeaderCustomDrawEventArgs e)
+        {
+            if (e.Column == null)
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void btn_StartScan_Click(object sender, EventArgs e)
+        {
+            using (PT_formMultiStart pt_formStart = new PT_formMultiStart(code_UserClubName))
+            {
+                if (pt_formStart.ShowDialog() == DialogResult.OK)
+                {
+                    if(gridControl1.DataSource == null)
+                    {
+                        dt = pt_formStart.getdtSchema.Clone();
+                        gridControl1.DataSource = dt;
+                        arrangeCol();
+                    }                                        
+                    dt.ImportRow(pt_formStart.getDR);
+                    gridView1.RefreshData();
+                }
+            }
+        }
+
+        DataTable dt = new DataTable();
+
+        private void btn_Done_Click(object sender, EventArgs e)
+        {
+            if (isFinger)
+            {
+                PT_fingerMultiStart pt_fingerMultiStart = new PT_fingerMultiStart(dt_finger_employees, lb_ClubName.Text, code_clubUser, gridView1.GetRowCellDisplayText(0,"productName"),dt );
+                if (pt_fingerMultiStart.ShowDialog() == DialogResult.OK)
+                {
+                    this.DialogResult = DialogResult.OK;
+                    this.Close();
+                }
+                else
+                {
+                    this.DialogResult = DialogResult.Cancel;
+                    this.Close();
+                }
+            }
+            //else
+            //{
+            //    PT_cardStart pt_cardstart = new PT_cardStart(lb_ClubName.Text, code_clubUser, gridView1.GetFocusedRowCellDisplayText("productName"), gridView1.GetFocusedRowCellDisplayText("remain"), gridView1.GetFocusedRowCellDisplayText("counter"), student_RGP, lb_Student_Name.Text);
+            //    if (pt_cardstart.ShowDialog() == DialogResult.OK)
+            //    {
+            //        this.DialogResult = DialogResult.OK;
+            //        this.Close();
+            //    }
+            //    else
+            //    {
+            //        this.DialogResult = DialogResult.Cancel;
+            //        this.Close();
+            //    }
+            //}
+
+            //List<ab> parts = new List<ab>();
+            //parts.Add(new ab { Name ="a", Kelas="b"});
+            //string asd = parts[0].Kelas;
+        }
+    }
+}
+
+//public class ab{
+    
+
+//    private string name; // field
+//    public string Name   // property
+//    {
+//        get { return name; }
+//        set { name = value; }
+//    }
+
+//    private string kelas; // field
+//    public string Kelas   // property
+//    {
+//        get { return kelas; }
+//        set { kelas = value; }
+//    }
+//}
